fc5a62c9932d644275fde6adfb6558dfae963449
Fleeman <tfleeman@MATL2232LTFLEE.attlocal.net>
2020-05-06 16:20:38 -0400

    working on simplecards

--- src/Components/MaterialUI/Cards/SimpleCardFunctional.js
+++ src/Components/MaterialUI/Cards/SimpleCardFunctional.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const data = [
+  {
+    title: 'Word of the Day',
+    word: 'be-nev-o-lent',
+    type: 'adjective',
+    meaning: 'well meaning and kindly.',
+    usage: 'a benevolent smile',
+  },
   {
     title: 'Word of the Day',
     word: 'be-nev-o-lent',
@@ -31,32 +38,39 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SimpleCardFunctional = () => {
+const SimpleCardFunctional = props => {
   const classes = useStyles();
+  const { loading = false } = props;
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          a benevolent smile
-        </Typography>
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-    </Card>
+    <Grid container spacing={2}>
+      {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+        <Grid item xs={12} md={6}>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                adjective
+              </Typography>
+              <Typography variant="body2" component="p">
+                well meaning and kindly.
+                <br />
+                a benevolent smile
+              </Typography>
+            </CardContent>
+            {/* <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions> */}
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
