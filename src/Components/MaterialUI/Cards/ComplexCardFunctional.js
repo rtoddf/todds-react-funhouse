cbad956eb3d4e738ac13c93e2d05ad81162b8c29
Fleeman <tfleeman@MATL2232LTFLEE.attlocal.net>
2020-05-08 15:51:00 -0400

    setup linter and playing with complex card with data

--- src/Components/MaterialUI/Cards/ComplexCardFunctional.js
+++ src/Components/MaterialUI/Cards/ComplexCardFunctional.js
@@ -1,9 +1,6 @@
 import React from "react";
 import clsx from 'clsx';
-import { Card, CardActions, CardContent, CardMedia, Collapse, Grid, Typography } from "@material-ui/core";
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, Grid, IconButton, Typography } from "@material-ui/core";
 
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
@@ -11,20 +8,35 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import { red } from '@material-ui/core/colors';
-
 import { makeStyles } from "@material-ui/core/styles";
+
+const data = [
+  {
+    name: "John Constantine",
+    source: "DC Comics",
+    media: "http://www.rtodd.net/images/comics-john-constantine.jpg",
+    bio: "The titular Hellblazer, Constantine is a working class warlock, occult detective and con man stationed in London. He is known for his endless cynicism, deadpan snarking, ruthless cunning and constant chain smoking, but he's also a passionate humanitarian driven by a heartfelt desire to do some good in his life.",
+    moreBio: "Although a compassionate humanist who struggles to overcome the influence of both Heaven and Hell over humanity, and despite his occasional forays into heroism, Constantine is a foul-mouthed, disillusioned, British cynic who pursues a life of sorcery and danger. His motivation has been attributed to an adrenaline addiction that only the strange and mysterious can sate. He also seems to be something of a \'Weirdness Magnet\'."
+  }
+]
 
 const useStyles = makeStyles((theme) => ({
   gridItem: {
     display: "flex",
   },
   root: {
-    maxWidth: 345,
+    // maxWidth: 345,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  cardHeader: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.primary.contrastText,
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
+
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
@@ -36,12 +48,33 @@ const useStyles = makeStyles((theme) => ({
     transform: 'rotate(180deg)',
   },
   avatar: {
-    backgroundColor: red[500],
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText
   },
+  // overrides: {
+    // Style sheet name ⚛️
+    MuiTypography: {
+      // Name of the rule
+      colorTextSecondary: {
+        // Some CSS
+        color: 'white',
+      },
+    },
+    MuiCardHeader: {
+      // Name of the rule
+      subheader: {
+        text: {
+          // Some CSS
+          color: 'white',
+        },
+      },
+    },
+  // },
 }));
 
 const ComplexCardFunctional = props => {
   const classes = useStyles();
+  const { loading = false } = props;
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -50,84 +83,62 @@ const ComplexCardFunctional = props => {
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={3} className={classes.gridItem}>
-        <Card className={classes.root}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                M
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
-            className={classes.media}
-            image="http://www.rtodd.net/images/comics-john-constantine.jpg"
-            title="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              This impressive paella is a perfect party dish and a fun meal to cook together with your
-              guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
+      {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+        <Grid item key={index} xs={12} md={3} className={classes.gridItem}>
+          <Card className={classes.root}>
+            <CardHeader
+              className={classes.cardHeader}
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  M
+                </Avatar>
+              }
+              // action={
+              //   <IconButton aria-label="settings">
+              //     <MoreVertIcon />
+              //   </IconButton>
+              // }
+              title={ item.name }
+              subheader={ item.source }
+            />
+            <CardMedia
+              className={classes.media}
+              image={ item.media }
+              title={ item.name }
+            />
             <CardContent>
-              <Typography paragraph>Method:</Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-              </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                minutes more. (Discard any mussels that don’t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              <Typography className={classes.bio} variant="body2" component="p">
+                { item.bio }
               </Typography>
             </CardContent>
-          </Collapse>
-
-        </Card>
-        
-      </Grid>
+            <CardActions disableSpacing>
+              {/* <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton> */}
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography className={classes.bio} variant="body2" component="p">
+                  { item.moreBio }
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Grid>
+      ))}
     </Grid>
-    )
+  )
 }
 
 export default ComplexCardFunctional;
