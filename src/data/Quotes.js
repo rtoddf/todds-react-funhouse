b65ddedb3e0b83234406abdfc3667ecdb66d6b98
Fleeman <tfleeman@MATL2232LTFLEE.attlocal.net>
2020-05-12 16:52:01 -0400

    added media/images to the data file for quotes

--- src/data/Quotes.js
+++ src/data/Quotes.js
@@ -1,31 +1,38 @@
 const quotes = [
   {
     quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author: "Nelson Mandela"
+    author: "Nelson Mandela",
+    media: "http://rtodd.net/images/dialog/nelson-mandela.jpg"
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
-    author: "Walt Disney"
+    author: "Walt Disney",
+    media: "http://rtodd.net/images/dialog/walt-disney.jpg"
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-    author: "Steve Jobs"
+    author: "Steve Jobs",
+    media: "http://rtodd.net/images/dialog/steve-jobs.jpg"
   },
   {
     quote: "If life were predictable it would cease to be life, and be without flavor.",
-    author: "Eleanor Roosevelt"
+    author: "Eleanor Roosevelt",
+    media: "http://rtodd.net/images/dialog/eleanor-roosevelt.jpg"
   },
   {
     quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-    author: "Oprah Winfrey"
+    author: "Oprah Winfrey",
+    media: "http://rtodd.net/images/dialog/oprah-winfrey.jpg"
   },
   {
     quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-    author: "James Cameron"
+    author: "James Cameron",
+    media: "http://rtodd.net/images/dialog/james-cameron.jpg"
   },
   {
     quote: "Life is what happens when you're busy making other plans.",
-    author: "John Lennon"
+    author: "John Lennon",
+    media: "http://rtodd.net/images/dialog/john-lennon.jpg"
   }
 ];
 
