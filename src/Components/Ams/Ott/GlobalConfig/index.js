433037c80703f1a3ec5ec18c9080c1fa6ace283f
Todd Fleeman <rtoddf@me.com>
2020-07-23 14:08:42 -0400

    global config page done

--- src/Components/Ams/Ott/GlobalConfig/index.js
+++ src/Components/Ams/Ott/GlobalConfig/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, CardHeader, Container, Grid, TextField, Typography } from "@material-ui/core";
 
 import { makeStyles } from '@material-ui/core/styles';
+import { ottGlobalConfig } from "../../data/OttGlobalConfig";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,20 +19,54 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const GlobalConfig = () => {
+const GlobalConfig = () => {;
   const classes = useStyles();
-  const [selected, setSelected] = React.useState(false)
+
+  const configItemsStructure = ottGlobalConfig.map((item, i) => {
+    return(
+      <>
+        Field
+      </>
+    );
+  });
 
   return (
-    <Container className={classes.root}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12}>
-          <Typography variant="h3" gutterBottom>
-            OTT Global Config
-          </Typography>
+    <>
+      <Container className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12}>
+            <Typography variant="h3" gutterBottom>
+              OTT Global Config
+            </Typography>
+          </Grid>
+          <Grid item md={6}>
+            <Card>
+              {/* <CardHeader
+                classes=""
+                title={ `Editing ${title}` }
+              /> */}
+              <CardContent>
+                {configItemsStructure}
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Container className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item md={3} className={classes.centered}>
+            <Button className={classes.button}>
+                Cancel
+            </Button>
+          </Grid>
+          <Grid item md={3} className={classes.centered}>
+            <Button className={classes.button}>
+                Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   )
 }
 
